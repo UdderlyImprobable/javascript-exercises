950ab4a9cd281d5e2ed4b2979a1baf0f6d725f7e
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-19 08:13:22 +0100

    Successfully complete the reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let i = 2;
+  let j = 1;
+  let newString = string.slice(-1);
+  while (i <= string.length) {
+    newString += string.slice(-i, -j);
+    i++
+    j++
+  }
+  return newString;
 };
 
 // Do not edit below this line
