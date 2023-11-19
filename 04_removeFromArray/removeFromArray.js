98975ddba3e8953f4ef2d405b7eb4374f1b92c3d
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-19 08:49:02 +0100

    Successfully complete removeFromArray test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remainingArgs) {
+  for(arg of remainingArgs) {
+    let i = 0;
+    while(i < arr.length) {
+      if (arr[i] ===  arg) {
+        arr.splice(i, 1);
+        break;
+      }
+      i++
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
