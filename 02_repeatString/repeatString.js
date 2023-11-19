af1ce2c9e8df95f056598c26f45a858f6714fbe6
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-19 07:36:28 +0100

    Successfully complete the repeatString.js test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let i = 0;
+  let repeatedString = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  while (i < num) {
+    repeatedString += string;
+    i++;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
