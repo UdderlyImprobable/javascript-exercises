eacc3eb4bc548c6de37fc5ada2a0a2e22b477973
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-21 14:38:26 +0100

    Successfully complete the calculator.js test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num, ...args) {
+	let remainder = num;
+  for (arg of args) {
+    remainder -= args;
+  }
+  return remainder;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (arg of arr) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let answer = 1;
+  for(arg of arr) {
+    answer *= arg;
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	let i = 0;
+  let answer = 1;
+  while(i < pow) {
+    answer *= base;
+    i++;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1;
+  for(let i = num; i >= 1; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
