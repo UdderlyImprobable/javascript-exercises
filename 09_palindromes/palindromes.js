a4ae6d974232038b1ca63e1f58692b2d4a1d3d2e
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2023-11-22 04:09:47 +0100

    Successfully complete the palindromes.js test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+  let string = str.toUpperCase().replace(/[^A-Z0-9]/g, "");
+  return string.split("").reverse().join("") === string;
+  
+}
 
 // Do not edit below this line
 module.exports = palindromes;
